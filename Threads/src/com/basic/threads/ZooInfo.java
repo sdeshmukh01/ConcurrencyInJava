07b87ddf18762c7ca49336cf299c4366fd87fc85
sdeshmukh01 <sagar.deshmukh4@gmail.com>
2018-03-06 02:39:20 +0530

    added first files

---
+++ Threads/src/com/basic/threads/ZooInfo.java
@@ -0,0 +1,29 @@
+package com.basic.threads;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * Created by himu on 3/5/2018.
+ */
+public class ZooInfo {
+
+   private static ExecutorService service;
+    public static void main(String[] args) {
+        try{
+        service = Executors.newSingleThreadExecutor();
+
+        System.out.println("begin");
+        service.execute(() -> System.out.println("Printing zoo inventory"));
+        service.execute(() -> {
+            for(int i =0; i < 3; i++) System.out.println("Printing record "+i);
+        });
+        service.execute(()->{
+            System.out.println("Printing zoo inventory");
+        });
+        System.out.println("end");
+
+    }catch(Exception e){
+        if(service != null) service.shutdown();
+    }
+}}
